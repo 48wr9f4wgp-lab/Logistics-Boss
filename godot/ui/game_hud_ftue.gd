d2f51693d1ae4f862662bb149f23e8af7f977797
48wr9f4wgp-lab <48wr9f4wgp@privaterelay.appleid.com>
2026-09-15 23:09:51 +0900

    Relay FTUE telemetry from HUD

--- godot/ui/game_hud_ftue.gd
+++ godot/ui/game_hud_ftue.gd
@@ -1,6 +1,9 @@
 extends "res://ui/game_hud_rank3.gd"
 class_name FtueGameHud
 
+signal ftue_step_changed(step_key: String)
+signal ftue_completed(skipped: bool)
+
 const FtueCoachScript = preload("res://ui/ftue_coach.gd")
 
 var _ftue_coach: LogisticsFtueCoach
@@ -14,6 +17,8 @@ func bind_sim(next_sim: WarehouseSim) -> void:
 func _ready() -> void:
     super._ready()
     _ftue_coach = FtueCoachScript.new()
+    _ftue_coach.step_changed.connect(_on_ftue_step_changed)
+    _ftue_coach.completed.connect(_on_ftue_completed)
     add_child(_ftue_coach)
     _bind_ftue_if_ready()
 
@@ -23,3 +28,11 @@ func _bind_ftue_if_ready() -> void:
         return
     var policy_buttons: Array[Button] = [_flow_button, _inbound_button, _outbound_button]
     _ftue_coach.bind_context(sim, _manage_button, policy_buttons, _sheet)
+
+
+func _on_ftue_step_changed(step_key: String) -> void:
+    ftue_step_changed.emit(step_key)
+
+
+func _on_ftue_completed(skipped: bool) -> void:
+    ftue_completed.emit(skipped)
